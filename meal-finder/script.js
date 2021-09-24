5d2d8ac3909293cc605f6411fb079f77811021c2
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-09-24 15:40:00 -0400

    started on meal finder

---
+++ meal-finder/script.js
@@ -0,0 +1,31 @@
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const random = document.getElementById("random");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const single_mealEl = document.getElementById("single-meal");
+
+// search meal and fetch from api
+function searchMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = "";
+
+  // Get search term
+  const term = search.value;
+
+  // Check for empty search
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+// event listeners
+submit.addEventListener("submit", searchMeal);
